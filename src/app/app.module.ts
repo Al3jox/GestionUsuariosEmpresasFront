6afa2c7e0a47e6e8d12fd4f4446ec77ee5b2a7e3
Diego Alejandro Muñoz A <diego.ch.84@hotmail.com>
2022-11-09 20:05:44 -0500

    Inicio del front, ya tiene compomentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { BarraLateralComponent } from './componentes/barra-lateral/barra-lateral.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BarraLateralComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
